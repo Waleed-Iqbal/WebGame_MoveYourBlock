5adfdfdd381cf3e2d4a9456a1e89729f02571f0c
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2018-06-24 21:56:37 +0500

    Basically renders animation+design

--- gameLogic.js
+++ gameLogic.js
@@ -1,5 +1,32 @@
+var width = 1500;
+var height = 700;
+
+function printChar() { // :D
+
+    if (x >= width) {
+        spdx = -spdx;
+    } else if (x <= 0)
+        spdx = Math.abs(spdx);
+    else if (y >= height) {
+        spdy = -spdy;
+    } else if (y <= 0)
+        spdy = Math.abs(spdy);
+
+    x += spdx;
+    y += spdy;
+    ctx.fillText("!", x, y);
+    //if (x >= width) clearInterval(printChar);
+}
 
 
 var ctx = document.getElementById("ctx").getContext("2d");
+var x = 50;
+var y = 50;
+var spdx = 10;
+var spdy = 20;
 
-ctx.fillText("Hello", 50, 50);
+
+
+ctx.font = "30px Arial";
+
+setInterval(printChar, 1);
